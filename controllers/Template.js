e5b1d5842b05616b74cf3f3fc64579402f570a5a
Thiago Souza <61321833+thiago-souza-mx@users.noreply.github.com>
2021-03-16 20:56:43 -0300

    Create Template.js

---
+++ controllers/Template.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Menu from "../componentes/Menu"; 
+import View  from "../views/View"; 
+
+export default class Template extends Component{
+  view = "../views/"+this.props.menu; 
+ 
+  render() {
+    return(
+      <div id="template">
+        <header></header>
+        <Menu id={this.props.menu}>       
+        </Menu>
+
+        <View page={this.props.view}>
+          {this.props.children}
+        </View>
+      </div>
+    );
+  }
+}
