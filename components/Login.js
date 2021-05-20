68cec74a6f282400720b97a305bfab06079750f3
thiago-souza-mx <thiagoraouza@gmail.com>
2021-05-20 07:17:23 -0300

    add select language

--- components/Login.js
+++ components/Login.js
@@ -16,26 +16,26 @@ export const Login = callback =>{
           </div>
           <div className="form-group py-3">
 
-            <input type="email" className="form-control px-3 radius font-20 p-2" id="proUser" />
+            <input type="email" className="form-control px-3 radius font-20 p-2" id="proUser" placeholder={Language({en:"Email", pt:"Email"})}/>
             
           </div>
           <div className="form-group">
 
-            <input type="password" className="form-control px-3 radius font-20 p-2" id="proPass" />
+            <input type="password" className="form-control px-3 radius font-20 p-2" id="proPass" placeholder={Language({en:"Password", pt:"Senha"})}/>
           </div>
           <div className="d-grid gap-2 pt-3">
-            <button type="submit"  onClick={callback} className="btn btn-block btn-primary radius btn-lg"></button>
+            <button type="submit"  onClick={callback} className="btn btn-block btn-primary radius btn-lg">{Language({en:"Signin", pt:"Entrar"})}</button>
           </div>
           <a href="/recover" className="text-center py-3 link-primary">
-           
+            <Language en="Recover Password" pt="Recuperar Senha"/>
           </a>
 
           <div className="text-center py-3">
 
-            
+            <Language en="No registration " pt="Não possui cadastro "/>
 
             <a href="/register" className="text-center px-3 btn btn-primary btn-sm radius">
-              
+              <Language en="Register" pt="Cadastrar"/>
             </a>
 
           </div>
@@ -48,7 +48,13 @@ export const Login = callback =>{
 
       <div id="languages" className="d-flex justify-content-center align-items-center">
         <Language en="Languages" pt="Idiomas"/>
+        <div className="lang-btn">
+          <PT/>
+        </div>
 
+        <div className="lang-btn">
+          <EN/>
+        </div>
       </div>
     </div>
   );
