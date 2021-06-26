fcca7c9cd99281a05566ad92f410fab0723b7b8f
thiago-souza-mx <thiagoraouza@gmail.com>
2021-06-26 01:42:07 -0300

    add particles

--- components/Register.js
+++ components/Register.js
@@ -2,6 +2,7 @@ import LogoSVG from "./LogoSVG"
 import {PT,Language, EN} from "./Language"
 import React from "react"
 import { fetchApi } from "../controllers/Auth";
+import Particles from 'react-particles-js';
 
 export default class Register extends React.Component{
 
@@ -21,6 +22,7 @@ export default class Register extends React.Component{
       }
     };
 
+    this.particlesModel = require("../model/particles.model");
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleMessage = this.handleMessage.bind(this);
@@ -67,6 +69,17 @@ export default class Register extends React.Component{
     return(
       <div id="__app_login" className="d-flex flex-column col-12 justify-content-center align-items-center">
 
+        <Particles 
+          style={{    
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            top: 0,
+            left: 0,
+          }}
+
+          params={this.particlesModel.json}
+        />
         <div  className={`alert alert-${this.state.alert.role} msg py-1`} style={{display:this.state.alert.display, zIndex:100}} role="alert">
           {this.state.alert.msg}
         </div>
